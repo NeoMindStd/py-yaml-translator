8e2c875f75be87cd5c0cc71679e1f02f92522921
NeoMindStd <dwj1996@naver.com>
2023-04-28 13:14:34 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# py-yaml-translator
-A Python tool to automatically translate YAML files using Google Translate. Supports multi-threading for faster translation.
+# YAML Translator
+
+This project uses the Google Translate API to automatically translate the content of a YAML file from one language to another. 
+
+## Installation
+
+1. Clone this repository to your local machine.
+
+```bash
+git clone https://github.com/yourusername/your-repository.git
+```
+
+2. Navigate to the project directory.
+
+```bash
+cd your-repository
+```
+
+3. Install the required Python packages.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the script `translate_yaml.py` with the following command-line arguments:
+
+- `-i` or `--input`: The path to the input YAML file.
+- `-o` or `--output`: The path to the output YAML file.
+- `-s` or `--source`: The source language (e.g., 'en' for English).
+- `-t` or `--target`: The target language (e.g., 'ko' for Korean).
+- `-w` or `--workers`: The number of worker threads to use for parallel processing.
+
+```bash
+python translate_yaml.py -i input.yml -o output.yml -s en -t ko -w 5
+```
+
+In the above example, the script will translate the content of `input.yml` from English to Korean and save the result in `output.yml`. It will use 5 worker threads for parallel processing.
+
+## Supported Languages
+
+The supported languages are those supported by the Google Translate API. For a full list of supported languages, refer to the [Google Translate API documentation](https://cloud.google.com/translate/docs/languages).
+
+## Note
+
+Due to the limitations of the free tier of the Google Translate API, there may be a delay in the translation process if the number of requests exceeds the limit.
+
+## License
+
+[This project is licensed under the terms of the MIT license.](./LICENSE)
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please note that this project is released with a Contributor Code of Conduct. By participating in this project, you agree to abide by its terms.
---
+++ translate_yaml.py
@@ -0,0 +1,68 @@
+#####################################################################################################################
+# Author: NeoMindStd                                                                                                #
+#   suppported by chat gpt (gpt-4)                                                                                  #
+#                                                                                                                   #
+# If there are too many requests from the same IP in a short period of time,                                        #
+# you may receive a 429 error code when requesting a translation for a while. In that case, please take a break.    #
+#####################################################################################################################
+
+import argparse
+from googletrans import Translator
+from ruamel.yaml import YAML
+from concurrent.futures import ThreadPoolExecutor
+
+translator = Translator(service_urls=['translate.google.com'])
+translator.raise_Exception = True
+
+def translate_text(key, text, target, source):
+    try:
+        result = translator.translate(text, dest=target, src=source)
+        # Remove any newline characters from the translated text
+        result_text = result.text
+        return key, result_text.replace('\n', ' ')
+    except Exception as e:
+        print(f"Error translating key: {key}, text: {text}. Error: {e}")
+        return key, text
+
+def translate_yaml(input_file, output_file, source_lang, target_lang, workers):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    with open(input_file, 'r', encoding='utf-8') as stream:
+        try:
+            yaml_content = yaml.load(stream)
+        except Exception as exc:
+            print(exc)
+
+    translated_content = {}
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        futures = []
+        for key, value in yaml_content.items():
+            if isinstance(value, list):
+                for i in range(len(value)):
+                    futures.append(executor.submit(translate_text, key, value[i], target_lang, source_lang))
+            elif isinstance(value, str):
+                futures.append(executor.submit(translate_text, key, value, target_lang, source_lang))
+
+        for future in futures:
+            result = future.result()
+            if result:
+                key, value = result
+                translated_content[key] = value
+
+    for key in yaml_content.keys():
+        if key in translated_content:
+            yaml_content[key] = translated_content.get(key)
+
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        yaml.dump(yaml_content, outfile)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Translate YAML file content')
+    parser.add_argument('-i', '--input', required=True, help='Input file path')
+    parser.add_argument('-o', '--output', required=True, help='Output file path')
+    parser.add_argument('-s', '--source', required=True, help='Source language')
+    parser.add_argument('-t', '--target', required=True, help='Target language')
+    parser.add_argument('-w', '--workers', type=int, default=1, help='Number of worker threads')
+    args = parser.parse_args()
+
+    translate_yaml(args.input, args.output, args.source, args.target, args.workers)
